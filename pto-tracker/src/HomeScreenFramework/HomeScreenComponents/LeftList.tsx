a15c9f8d34064193b8503c84ffdfcc5927e557ae
RobStarkie <rc.starkie@yahoo.co.uk>
2023-11-13 10:52:57 +0000

    created the framework for the homepage

---
+++ pto-tracker/src/HomeScreenFramework/HomeScreenComponents/LeftList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './LeftList.css'
+
+interface LeftListProps {
+    content: string;
+}
+
+const LeftList: React.FC<LeftListProps> = ({ content }) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LeftList;
